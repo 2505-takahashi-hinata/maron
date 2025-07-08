e624de64f26d6829956b918affe64e792f26e938
旭廉 <asahi.ren@alhinc.jp>
2025-07-08 15:33:10 +0900

    15:32-0708asahi

--- src/main/java/com/example/maron/controller/UserManageController.java
+++ src/main/java/com/example/maron/controller/UserManageController.java
@@ -43,9 +43,10 @@ public class UserManageController {
         int loginUserId = loginUser.getId();
         //エラーをsessionから取得
         List<String> errors =(List<String>)session.getAttribute("errors");
-        if(errors != null){
-            mav.addObject("errors",errors);
+        if(errors != null) {
+            mav.addObject("errors", errors);
             session.removeAttribute("errors");
+        }
         // 画面遷移先を指定
         mav.setViewName("/user");
         // 準備した空のFormを保管
